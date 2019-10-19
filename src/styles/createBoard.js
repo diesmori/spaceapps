3cb79b6b2687c081fc408cafafade81beccd848d
diesmori <diesmori@gmail.com>
2019-10-19 16:21:05 -0300

    QR posting

---
+++ src/styles/createBoard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import { colors } from "./colors";
+
+export const createBoard = StyleSheet.create({
+
+  background: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center"
+  }
+
+});
